0f221bccdceb95b7b36633bec71af1bedc7b139e
Karen17Mendoza <melanie171095@gmail.com>
2025-01-21 15:47:24 -0500

    adds changes in the file

---
+++ app.js
@@ -0,0 +1,10 @@
+let numeroSecreto = 6;
+let numeroUusuario = prompt("Me indicas un numero entre 1 y 10:");
+
+console.log(numeroUusuario);
+
+if(numeroUusuario == numeroSecreto) {
+    alert("Acertaste el numero!");
+} else {
+    alert("Lo siento, no acertaste el numero");
+}
